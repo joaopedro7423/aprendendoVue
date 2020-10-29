5f2cb689791c378f18554ddba6b61559d4b08d83
Joao Pedro <802.018@alunos.unigran.br>
2020-10-29 10:34:55 -0300

    Como usar o vue no front

--- guiaclientes/src/App.vue
+++ guiaclientes/src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h3>Cadastro</h3>
+    <input type="text" placeholder="nome" v-model="nomeField"><br>
+    <input type="email" placeholder="email" v-model="emailField"><br>
+    <input type="number" placeholder="idade" v-nome="idadeField"><br> 
+    <button @click="cadastrarUsuarios">Cadastrar</button>   
+
+
+    <div v-for="(cliente,index) in clientes" :key="cliente.id">
+      <h1>{{index}}</h1>
+     <Cliente :cliente="cliente"/>
+      <input type="text" v-model="cliente.nome">
+    </div>
+   
+     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cliente from './components/Cliente'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name:'App',
+    data(){
+      return{
+          nomeField:"",
+          emailField:"",
+          idadeField:0,
+      clientes:[
+        {
+          id:1,
+          nome:"JOOJ PEREIRA",
+          email:"jooj@jooj.saa",
+          idade: 99
+        },{
+          id:2,
+ nome:"JOOJ PEREIR2A",
+          email:"jooj2@jooj.saa",
+          idade: 992
+        },{
+          id:3,
+ nome:"JOOJ PEREI213R2A",
+          email:"jooj3212@jooj.saa",
+          idade: 992132
+        }
+      ]
+          }
+},
+    components:{
+      Cliente
+    },
+    methods:{
+      cadastrarUsuarios: function(){
+        this.clientes.push({nome: this.nomeField,email: this.emailField, idade: this.idadeField, id:Date.now()})
+        this.nomeField="";
+        this.emailField="";
+        this.idadeField=0;
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,80 @@
+<template>
+        <div :class="{'cliente': !isPremium,'cliente-premium': isPremium}" >
+      <!--  <input type="text" v-model="nome">
+                <hr>
+                <input type="text" v-model="descricao">
+        <h2 id="cliente-nome">Cliente : {{nome}}</h2>
+        <h3>Descrição : {{descricao}}</h3>
+  
+        <h4>Nome: {{cliente.nome}}</h4>
+        <hr>
+        <p>{{descricao}}</p>
+        <hr>
+        <p>Número: {{numero}}</p>
+        <p>Email: {{cliente.email}}</p>
+        <p>Idade :{{cliente.idade}}</p>
+        <p v-if="showIdade == true">Idade :{{cliente.idade}}</p>
+  -->
+
+        <h4>Nome: {{cliente.nome}}</h4>
+        <hr>
+        
+        <p>Email: {{cliente.email}}</p>
+         <p>Idade: {{cliente.idade}}</p>
+        <button @click="mudarCor($event)">Mudar Cor</button>
+    </div>
+</template>
+
+<script>
+export default {
+        data(){
+                return{
+                     isPremium: false
+                     //   numero:"24",
+                      
+                   //     descricao:"era um maluco muito doido",
+                     //   idade:20
+                }
+                
+        },props:{
+                 //nome: String,
+                 //email: String,
+                 //idade: Number,
+                 cliente: Object,
+                 showIdade:Boolean    
+                },
+                methods:{
+                        mudarCor: function($event){
+                                console.log($event);
+                                this.isPremium = !this.isPremium;
+                           /*if(this.isPremium == false){
+                                      this.isPremium = true;
+                              }else{
+                                        this.isPremium = false;  
+                              }
+                              */
+                        }
+                }
+}
+</script>
+
+<style scoped>
+        .cliente{
+               background-color: #ECE5E3;
+                max-width: 600px;
+                height: 250px;
+                padding: 1%;
+                margin-top: 2%;
+        }
+
+        .cliente-premium{
+                background-color: black;
+                color: yellow;
+                max-width: 600px;
+                height: 250px;
+                padding: 1%;
+                margin-top: 2%;
+        }
+
+       
+</style>
